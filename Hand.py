113da6af04a27f770ffe9b23738cc961037ae6f3
Adedeji Adepetun <dejiadepetun@gmail.com>
2018-07-26 05:49:42 +0100

    added random hand generator with declaration target to the Hand class

--- Hand.py
+++ Hand.py
@@ -29,11 +29,11 @@ class Hand:
     def __compute_stats(self):
         return compute_point(self), compute_sequences(self), compute_sets(self)
 
-    def remove_cards(self, cards):
-        self.cards = [card for card in self.cards if card not in cards]
+    def remove_cards(self, card):
+        self.cards.remove(card)
 
-    def add_cards(self, cards):
-        self.cards += cards
+    def add_card(self, card):
+        self.cards.append(card)
         self.points, self.sequences, self.sets = self.__compute_stats()
 
     def get_point_value(self):
@@ -83,44 +83,92 @@ class Hand:
     def get_remaining_cards(self):
         return [card for card in Deck().cards if card not in self.cards]
 
-    def add_point_increasing_card(self):
+    def get_point_increasing_cards(self, possible_cards):
         suit_points = {card.suit: len([c for c in self.cards if c.suit == card.suit]) for card in self.cards}
         point_sorted_suit = [p[0] for p in sorted(suit_points.items(), key=lambda x: x[1], reverse=True)]
+        result = []
         for suit in point_sorted_suit:
-            for card in [c for c in self.get_remaining_cards() if c.suit == suit]:
+            for card in [c for c in possible_cards if c.suit == suit]:
                 temp = Hand(deepcopy(self.cards))
                 temp.cards.append(card)
                 if temp.get_sequence_value() == self.get_sequence_value() \
                         and temp.get_set_value() == self.get_set_value():
-                    self.cards.append(card)
-                    self.points, self.sequences, self.sets = self.__compute_stats()
-                    return
+                    result.append(card)
 
-    def add_sequence_increasing_card(self):
+        return result
+
+    def get_sequence_increasing_cards(self, possible_cards):
         suit_seqs = {suit: max([list(g)
                                 for g in consecutive_groups(
                 sorted([card.rank for card in hand.cards if card.suit == suit]))],
                                key=lambda g: len(g)) for suit in {card.suit for card in self.cards}}
         seq_sorted_suits = [p[0] for p in sorted(suit_seqs.items(), key=lambda x: len(x[1]), reverse=True)]
 
+        result = []
         for suit in seq_sorted_suits:
-            cards = sorted([card.rank for card in self.cards if card.suit == suit])
-            missing = sorted(set(range(1, 9)).difference(cards))
+            suit_cards = sorted([card.rank for card in self.cards if card.suit == suit])
+            missing = sorted(set(range(1, 9)).difference(suit_cards))
             sub_seqs = {}
             for c in missing:
-                temp = deepcopy(cards)
+                temp = deepcopy(suit_cards)
                 temp.append(c)
                 sub_seqs[c] = get_longest_sub_sequence(sorted(temp))
-            sub_seqs_sorted = [p[0] for p in sorted(sub_seqs.items(), key=lambda x: x[1], reverse=True)]
+            sub_seqs_sorted = [p[0] for p in sorted(sub_seqs.items(), key=lambda x: (x[1], x[0]), reverse=True)]
             for rank in sub_seqs_sorted:
-                for card in [card for card in self.get_remaining_cards() if card.suit == suit]:
+                for card in [card for card in possible_cards if card.suit == suit]:
                     if card.rank == rank:
                         temp = Hand(deepcopy(self.cards))
                         temp.cards.append(card)
-                        if temp.get_point_value() == self.get_point_value() and temp.get_set_value() == self.get_set_value():
-                            self.cards.append(card)
-                            self.points, self.sequences, self.sets = self.__compute_stats()
-                            return
+                        if temp.get_point_value() == self.get_point_value() and \
+                                temp.get_set_value() == self.get_set_value():
+                            result.append(card)
+
+        return result
+
+    def get_set_increasing_cards(self, possible_cards):
+        rank_sets = {rank: len([card for card in self.cards if card.rank == rank])
+                     for rank in {card.rank for card in self.cards if card.rank in range(4, 9)}}
+        set_sorted_ranks = [p[0] for p in sorted(rank_sets.items(), key=lambda x: (x[1], x[0]), reverse=True)]
+        result = []
+        for rank in set_sorted_ranks:
+            for card in [card for card in possible_cards if card.rank == rank]:
+                temp = Hand(deepcopy(self.cards))
+                temp.cards.append(card)
+                if temp.get_point_value() == self.get_point_value() and \
+                        temp.get_sequence_value() == self.get_sequence_value():
+                    result.append(card)
+
+        return result
+
+    def get_impotent_card(self, possible_cards):
+        impotent_cards = []
+        for card in possible_cards:
+            temp = Hand(deepcopy(self.cards))
+            temp.cards.append(card)
+            if temp.get_point_value() == self.get_point_value() \
+                    and temp.get_sequence_value() == self.get_sequence_value() \
+                    and temp.get_set_value() == self.get_set_value():
+                impotent_cards.append(card)
+        return impotent_cards if impotent_cards else possible_cards
+
+    def add_random_card(self, stat_target, possible_cards):
+        addable_cards = deepcopy(possible_cards)
+        if stat_target['point']:
+            addable_cards = [
+                card for card in addable_cards if card in self.get_point_increasing_cards(addable_cards)]
+        if stat_target['sequence']:
+            addable_cards = [
+                card for card in addable_cards if card in self.get_sequence_increasing_cards(addable_cards)]
+        if stat_target['set']:
+            addable_cards = [
+                card for card in addable_cards if card in self.get_set_increasing_cards(addable_cards)]
+        if not any(stat_target.values()):
+            self.add_card(random.choice(self.get_impotent_card(possible_cards)))
+        else:
+            if not addable_cards:
+                self.add_card(random.choice(self.get_impotent_card(possible_cards)))
+            else:
+                self.add_card(addable_cards[0])
 
     def __repr__(self):
         return str(self.cards)
@@ -132,8 +180,8 @@ class Hand:
 if __name__ == "__main__":
     cards = []
     cards.append(Card(3, 'D'))
-    cards.append(Card(7, 'C'))
-    cards.append(Card(4, 'C'))
+    cards.append(Card(3, 'S'))
+    cards.append(Card(1, 'C'))
     cards.append(Card(1, 'H'))
     cards.append(Card(4, 'H'))
     cards.append(Card(5, 'C'))
@@ -144,11 +192,26 @@ if __name__ == "__main__":
     cards.append(Card(6, 'H'))
     cards.append(Card(1, 'C'))
 
-    cards = Deck().cards[:12]
+    cards = Deck().cards[:0]
 
     hand = Hand(cards)
-    print(hand.cards)
-    print(hand.sequences)
-    hand.add_sequence_increasing_card()
-    print(hand.cards)
-    print(hand.sequences)
+    target = {'point': 5, 'sequence': 0, 'set': 3}
+
+    while len(hand.cards) < 12:
+        stats = {'point': hand.get_point_value() != target['point'] if target['point'] > 0 else False,
+                 'sequence': hand.get_sequence_value() != target['sequence'] if target['sequence'] > 0 else False,
+                 'set': hand.get_set_value() != target['set'] if target['set'] > 0 else False}
+        hand.add_random_card(stats, hand.get_remaining_cards())
+        if (hand.get_point_value() > target['point'] and stats['point']) or \
+                (hand.get_set_value() > target['set'] and stats['set']) or \
+                (hand.get_sequence_value() > target['sequence'] and stats['sequence']):
+            cards = [card for card in hand.points + hand.get_best_set() + hand.get_best_sequence() if card not in cards]
+            if cards:
+                hand.remove_cards(random.choice(cards))
+            else:
+                hand.remove_cards(random.choice(hand.cards))
+
+    print('point: ', hand.get_point_value())
+    print('sequence: ', hand.get_sequence_value())
+    print('set: ', hand.get_set_value())
+    print(len(hand.cards))
