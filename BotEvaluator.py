a02b5f64d148584214d5da2ad0f1e3c321d8e36b
Adedeji Adepetun <dejiadepetun@gmail.com>
2018-09-01 18:15:33 +0100

    modified create stats sql query to return repique, pique, cards and capot percentages

--- BotEvaluator.py
+++ BotEvaluator.py
@@ -120,20 +120,20 @@ def create_stats_table(conn, bots, filename):
 
 
 if __name__ == "__main__":
-    for i in range(100):
-        games = 8
+    for i in range(1):
+        games = 1
         explorations = []
         explorations.append([1/sqrt(2), 1/sqrt(2)])
-        explorations.append([1/sqrt(2), 0.1])
-        explorations.append([1/sqrt(2), 0.5])
-        explorations.append([1/sqrt(2), 1])
-        explorations.append([1/sqrt(2), 2])
-        explorations.append([1/sqrt(2), 2.5])
-        explorations.append([1/sqrt(2), 5])
-        explorations.append([1/sqrt(2), 7.5])
-        explorations.append([1/sqrt(2), 10])
-        explorations.append([1/sqrt(2), 15])
-        explorations.append([1/sqrt(2), 20])
+        # explorations.append([1/sqrt(2), 0.1])
+        # explorations.append([1/sqrt(2), 0.5])
+        # explorations.append([1/sqrt(2), 1])
+        # explorations.append([1/sqrt(2), 2])
+        # explorations.append([1/sqrt(2), 2.5])
+        # explorations.append([1/sqrt(2), 5])
+        # explorations.append([1/sqrt(2), 7.5])
+        # explorations.append([1/sqrt(2), 10])
+        # explorations.append([1/sqrt(2), 15])
+        # explorations.append([1/sqrt(2), 20])
         # explorations.append([1/sqrt(2), 0.6])
         # explorations.append([1/sqrt(2), 0.65])
         # explorations.append([1/sqrt(2), 0.8])
@@ -142,21 +142,25 @@ if __name__ == "__main__":
         time_resources = [1]
 
         bot_names = []
-        # bot_names.append(['kbs', 'random'])
-        # bot_names.append(['absolute_result', 'score_strength'])
-        # bot_names.append(['kbs', 'score_strength'])
-        # bot_names.append(['random', 'score_strength'])
-        # bot_names.append(['absolute_result', 'random'])
-        # bot_names.append(['absolute_result', 'kbs'])
+        bot_names.append(['kbs', 'random'])
+        bot_names.append(['absolute_result', 'score_strength'])
+        bot_names.append(['kbs', 'score_strength'])
+        bot_names.append(['random', 'score_strength'])
+        bot_names.append(['absolute_result', 'random'])
+        bot_names.append(['absolute_result', 'kbs'])
         # bot_names.append(['random1', 'random2'])
         # bot_names.append(['absolute_result', 'absolute_result_history'])
         # bot_names.append(['absolute_result_cheat', 'random'])
         # bot_names.append(['kbs1', 'kbs2'])
         # bot_names.append(['absolute_result1', 'absolute_result2'])
-        bot_names.append(['absolute_result', 'better_score_strength'])
-        bot_names.append(['absolute_result', 'average_score_strength'])
+        # bot_names.append(['absolute_result', 'better_score_strength'])
+        # bot_names.append(['absolute_result', 'average_score_strength'])
+        conn = create_connection(db['file'])
+        with conn:
+            for bot in bot_names:
+                create_stats_table(conn, bot, 'data/get_deal_stats_from_table.txt')
 
-        for e in explorations:
-            for j in range(len(bot_names)):
-                for time in time_resources:
-                    evaluate_bots_parallel(bot_names[j], db, games, time, e)
+        # for e in explorations:
+        #     for j in range(len(bot_names)):
+        #         for time in time_resources:
+        #             evaluate_bots_parallel(bot_names[j], db, games, time, e)
